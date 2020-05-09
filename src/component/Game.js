c2a43602fe4daaa82d4af70181851220d9466fb8
1132008 <nrak15@gmail.com>
2020-05-09 19:16:13 +0530

    refactor: Extracted magic numbers into constant

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Tile from './Tile';
 import '../App.css';
+import Constants from '../constants/Constants';
 import StyleConstants from '../constants/StyleConstants';
 
 const Game = () => {
 
     const renderBoard = () => {
         let tileList = [];
-        for (let position = 0; position < 9; position++) {
+        for (let position = Constants.INITIAL_TILE_POSITION; position < Constants.NUMBER_OF_TILES; position++) {
             tileList.push(<li key={position}>
                 <Tile />
             </li>);
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,4 +1,6 @@
 const Constants = {
-    APP_TITLE: "Tic-Tac-Toe"
+    APP_TITLE: "Tic-Tac-Toe",
+    NUMBER_OF_TILES: 9,
+    INITIAL_TILE_POSITION: 0
 };
 export default Constants;
